7d15af737e87fbd802c0d8ec3328124c7e580439
Afsoon <said.atrahouch@gmail.com>
2020-12-17 11:15:19 +0100

    WIP Remove Phone dialog modal

--- assets/src/pages/Detail.tsx
+++ assets/src/pages/Detail.tsx
@@ -4,6 +4,12 @@ import { Header } from "../components/Header"
 import Text from "../components/Text"
 import AppShell from "../components/AppShell"
 import { Footer } from "../components/Footer"
+import {
+  useDialogState,
+  Dialog,
+  DialogDisclosure,
+  DialogBackdrop,
+} from "reakit/Dialog"
 
 const getPhones = async () => await fetch("http://localhost:3000/phones", {})
 
@@ -16,14 +22,90 @@ const HeaderDetail = () => {
     </Header>
   )
 }
+
 const DeletePhone = () => {
+  const dialog = useDialogState()
   return (
-    <Button
-      type="button"
-      className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-    >
-      Borrar teléfono
-    </Button>
+    <>
+      <DialogDisclosure
+        {...dialog}
+        className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Borrar teléfono
+      </DialogDisclosure>
+      <DialogBackdrop
+        {...dialog}
+        className="fixed z-10 inset-0 overflow-y-auto"
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+            <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+          </div>
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          ></span>
+          &#8203;
+          <Dialog
+            {...dialog}
+            role="alertdialog"
+            className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+            aria-labelledby="modal-headline"
+          >
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <svg
+                  className="h-6 w-6 text-red-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
+              </div>
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-headline"
+                >
+                  Borrar iPhone X del cátalogo
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    ¿Estas seguro de querer borrar el télefono del listado?.
+                    Esta operación no se puede deshacer
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 sm:mt-4 sm:ml-10 sm:pl-4 sm:flex">
+              <Button
+                onClick={() => {
+                  alert("Discarded")
+                  dialog.hide()
+                }}
+                className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:w-auto sm:text-sm"
+              >
+                Borrar
+              </Button>
+              <Button
+                onClick={dialog.hide}
+                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 px-4 py-2 bg-white text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Cancelar
+              </Button>
+            </div>
+          </Dialog>
+        </div>
+      </DialogBackdrop>
+    </>
   )
 }
 
