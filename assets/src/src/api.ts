553f294d8ec5e3a75be664450c39a2d7bcbc3577
Afsoon <said.atrahouch@gmail.com>
2020-12-22 16:50:54 +0100

    api prod

--- assets/src/src/api.ts
+++ assets/src/src/api.ts
@@ -2,7 +2,7 @@ let api = ""
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   api = "http://localhost:4000/api"
 } else {
-  api = process.env.API_URL || ""
+  api = "https://phone-catalog.gigalixirapp.com/api"
 }
 
 export const fetchApi = (path: string, options?: RequestInit | undefined) => {
