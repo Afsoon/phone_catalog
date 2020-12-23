b6180654141e1616aedc83c8b9851ffd2dda4252
Afsoon <said.atrahouch@gmail.com>
2020-12-23 08:43:00 +0100

    Added the missed fix for 100vh on iOS

--- assets/src/components/AppShell/index.tsx
+++ assets/src/components/AppShell/index.tsx
@@ -1,6 +1,6 @@
 const AppShell: React.FC = ({ children }) => {
   return (
-    <div className="overflow-hidden w-screen h-screen bg-gray-100 grid grid-rows-7">
+    <div className="overflow-hidden w-screen h-screen-ios bg-gray-100 grid grid-rows-7">
       {children}
     </div>
   )
